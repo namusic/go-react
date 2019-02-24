ae0f1c33c8833ea4c1f220f8c2d6776669e35a7f
naoto-yoshimoto <namusic7010@gmail.com>
2019-02-24 16:50:19 +0900

    implement backend(go)

--- rtsupport/go/main.go
+++ rtsupport/go/main.go
@@ -1,44 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
-var upgrater = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+
+type Channel struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":4040", nil)
-}
+	router := NewRouter()
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "hello from go")
-
-	socket, err := upgrater.Upgrade(w, r, nil)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		msgType, msg, err := socket.ReadMessage()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(string(msg))
-		if err = socket.WriteMessage(msgType, msg); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-	}
-
+	router.Handle("channel add", addChannel)
+	//	http.HandleFunc("/", handler)
+	http.Handle("/", router)
+	http.ListenAndServe(":4000", nil)
 }
